e926d52a8b1cc667f1ee4fdb95f318400ea7eada
Niklas Heidloff <niklas_heidloff@de.ibm.com>
2022-03-30 10:07:21 +0200

    injection error

--- database-service/src/main/java/heidloff/net/database/RemoteDatabaseService.java
+++ database-service/src/main/java/heidloff/net/database/RemoteDatabaseService.java
@@ -1,10 +1,12 @@
 package heidloff.net.database;
 
 import org.eclipse.microprofile.rest.client.inject.RegisterRestClient;
+import javax.enterprise.context.ApplicationScoped;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import java.util.Set;
 
+@ApplicationScoped
 @Path("/api")
 @RegisterRestClient
 public interface RemoteDatabaseService {
