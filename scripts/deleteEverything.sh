9c75b528c616f267120c890815d336a1b48a7358
thomassuedbroecker <thomas.suedbroecker@de.ibm.com>
2022-04-29 15:34:33 +0200

    prometheus related changes for the automated setup

--- scripts/deleteEverything.sh
+++ scripts/deleteEverything.sh
@@ -1,39 +1,157 @@
 #!/bin/bash
 
+# **************** Global variables
+
 ROOT_FOLDER=$(cd $(dirname $0); cd ..; pwd)
 
-cd $ROOT_FOLDER/operator-application
-kubectl delete -f config/samples/application.sample_v1beta1_application.yaml
-kubectl delete -f config/samples/application.sample_v1alpha1_application.yaml
+# **********************************************************************************
+# Functions
+# **********************************************************************************
 
-make undeploy IMG="$REGISTRY/$ORG/$IMAGE_APPLICATION_OPERATOR"
-operator-sdk cleanup operator-application -n operators --delete-all
+function setEnvironmentVariables () {
+    source $ROOT_FOLDER/versions.env
+}
 
-kubectl delete catalogsource operator-application-catalog -n operators 
-kubectl delete subscriptions operator-application-v0-0-1-sub -n operators 
-kubectl delete csv operator-application.v0.0.1 -n operators
-kubectl delete operators operator-application.operators -n operators
-kubectl delete installplans -n operators --all
+function deleteMicroserviceApplicationInstance () { 
+    cd $ROOT_FOLDER/operator-application
+    kubectl delete -f config/samples/application.sample_v1beta1_application.yaml
+    kubectl delete -f config/samples/application.sample_v1alpha1_application.yaml
+    #echo "Press any key to move on"
+    #read input
+}
 
-kubectl delete namespace application-alpha
-kubectl delete namespace application-beta
-kubectl delete all --all -n operator-application-system
-kubectl delete namespace operator-application-system
+function deleteApplicationOperator () {
+    make undeploy IMG="$REGISTRY/$ORG/$IMAGE_APPLICATION_OPERATOR"
+    operator-sdk cleanup operator-application -n operators --delete-all
+    #echo "Press any key to move on"
+    #read input
+}
 
-cd $ROOT_FOLDER/operator-database
-kubectl delete -f config/samples/database.sample_v1alpha1_database.yaml
+function deleteOLMdeployment () {
+    kubectl delete catalogsource operator-application-catalog -n operators 
+    kubectl delete subscriptions operator-application-v0-0-1-sub -n operators 
+    kubectl delete csv operator-application.v0.0.1 -n operators
+    kubectl delete operators operator-application.operators -n operators
+    kubectl delete installplans -n operators --all
+    #echo "Press any key to move on"
+    #read input
+}
 
-make undeploy IMG="$REGISTRY/$ORG/$IMAGE_DATBASE_OPERATOR"
-operator-sdk cleanup operator-database -n operators --delete-all
+function deleteNamespacesRelatedToApplicationOperator () {
+    kubectl delete namespace application-alpha
+    kubectl delete namespace application-beta
+    kubectl delete all --all -n operator-application-system
+    kubectl delete namespace operator-application-system
+    #echo "Press any key to move on"
+    #read input
+}
 
-kubectl delete namespace database
-kubectl delete all --all -n operator-database-system
-kubectl delete namespace operator-database-system
+function deleteDatabaseInstance () {
+    cd $ROOT_FOLDER/operator-database
+    kubectl delete -f config/samples/database.sample_v1alpha1_database.yaml
+    #echo "Press any key to move on"
+    #read input
+}
 
-cd $ROOT_FOLDER
-kubectl delete -f prometheus/prometheus/
-kubectl delete -f prometheus/operator/
+function deleteDatabaseOperator () {
+    make undeploy IMG="$REGISTRY/$ORG/$IMAGE_DATBASE_OPERATOR"
+    operator-sdk cleanup operator-database -n operators --delete-all
+    #echo "Press any key to move on"
+    #read input
+}
 
-operator-sdk olm uninstall
+function deleteNamespacesRelatedToDatabaseOperator () {
+    kubectl delete namespace database
+    kubectl delete all --all -n operator-database-system
+    kubectl delete namespace operator-database-system
+    #echo "Press any key to move on"
+    #read input
+}
 
-kubectl delete -f https://github.com/cert-manager/cert-manager/releases/download/v1.7.2/cert-manager.yaml
+function deletePrometheus () {
+    cd $ROOT_FOLDER
+    kubectl delete -f prometheus/prometheus/
+    kubectl delete -f prometheus/operator/
+    kubectl delete customresourcedefinition alertmanagerconfigs.monitoring.coreos.com
+
+    # delete crds
+    kubectl delete customresourcedefinition podmonitors.monitoring.coreos.com
+    kubectl delete customresourcedefinition servicemonitors.monitoring.coreos.com
+    kubectl delete customresourcedefinition thanosrulers.monitoring.coreos.com
+    kubectl delete customresourcedefinition prometheusrules.monitoring.coreos.com
+    kubectl delete customresourcedefinition prometheuses.monitoring.coreos.com
+    kubectl delete customresourcedefinition probes.monitoring.coreos.com
+    kubectl delete customresourcedefinition alertmanagers.monitoring.coreos.com
+    kubectl delete customresourcedefinition podmonitors.monitoring.coreos.com
+    
+    #echo "Press any key to move on"
+    #read input
+}
+
+function deleteOLM () {
+    operator-sdk olm uninstall
+    
+    #echo "Press any key to move on"
+    #read input
+}
+
+function deleteCertManager () {
+    kubectl delete -f https://github.com/cert-manager/cert-manager/releases/download/v1.7.2/cert-manager.yaml
+        
+    #echo "Press any key to move on"
+    #read input
+}
+
+# **********************************************************************************
+# Execution
+# **********************************************************************************
+
+echo "************************************"
+echo " Set environment"
+echo "************************************"
+setEnvironmentVariables
+
+echo "************************************"
+echo " Delete microservice application"
+echo "************************************"
+deleteMicroserviceApplicationInstance
+
+echo "************************************"
+echo " Delete application operator"
+echo "************************************"
+deleteApplicationOperator
+
+echo "************************************"
+echo " Delete application operator OLM deployment"
+echo "************************************"
+deleteOLMdeployment
+
+echo "************************************"
+echo " Delete namespaces related to application operator"
+echo "************************************"
+deleteNamespacesRelatedToApplicationOperator
+
+echo "************************************"
+echo " Delete database application and operator"
+echo "************************************"
+deleteDatabaseInstance
+
+echo "************************************"
+echo " Delete database application"
+echo "************************************"
+deleteNamespacesRelatedToDatabaseOperator
+
+echo "************************************"
+echo " Delete prometheus"
+echo "************************************"
+deletePrometheus
+
+echo "************************************"
+echo " Delete OLM"
+echo "************************************"
+deleteOLM
+
+echo "************************************"
+echo " Delete cert manager"
+echo "************************************"
+deleteCertManager
---
+++ scripts/install-required-kubernetes-components.sh
@@ -0,0 +1,172 @@
+#!/bin/bash
+
+# **************** Global variables
+
+ROOT_FOLDER=$(cd $(dirname $0); cd ..; pwd)
+
+# **********************************************************************************
+# Functions
+# **********************************************************************************
+
+function installCertManager () {
+  kubectl apply -f https://github.com/cert-manager/cert-manager/releases/download/v1.7.2/cert-manager.yaml
+  kubectl get pods -n cert-manager
+
+  array=("cert-manager-cainjector" "cert-manager-webhook")
+  export STATUS_SUCCESS="Running"
+  for i in "${array[@]}"
+    do 
+        echo ""
+        echo "------------------------------------------------------------------------"
+        echo "Check $i"
+        while :
+        do
+            FIND=$i
+            STATUS_CHECK=$(kubectl get pods -n cert-manager | grep "$FIND" | awk '{print $3;}' | sed 's/"//g' | sed 's/,//g')
+            echo "Status: $STATUS_CHECK"
+            STATUS_VERIFICATION=$(echo "$STATUS_CHECK" | grep $STATUS_SUCCESS)
+            if [ "$STATUS_VERIFICATION" = "$STATUS_SUCCESS" ]; then
+                echo "$(date +'%F %H:%M:%S') Status: $FIND is Ready"
+                echo "------------------------------------------------------------------------"
+                break
+            else
+                echo "$(date +'%F %H:%M:%S') Status: $FIND($STATUS_CHECK)"
+                echo "------------------------------------------------------------------------"
+            fi
+            sleep 3
+        done
+    done 
+}
+
+function installOLM () {
+  operator-sdk olm install --version v0.20.0
+  kubectl get pods -n operators
+  kubectl get pods -n olm
+
+  array=("catalog-operator" "olm-operator" "operatorhubio-catalog" )
+  namespace=olm
+  export STATUS_SUCCESS="Running"
+  for i in "${array[@]}"
+    do 
+        echo ""
+        echo "------------------------------------------------------------------------"
+        echo "Check $i"
+        while :
+        do
+            FIND=$i
+            STATUS_CHECK=$(kubectl get pods -n $namespace | grep "$FIND" | awk '{print $3;}' | sed 's/"//g' | sed 's/,//g')
+            echo "Status: $STATUS_CHECK"
+            STATUS_VERIFICATION=$(echo "$STATUS_CHECK" | grep $STATUS_SUCCESS)
+            if [ "$STATUS_VERIFICATION" = "$STATUS_SUCCESS" ]; then
+                echo "$(date +'%F %H:%M:%S') Status: $FIND is Ready"
+                echo "------------------------------------------------------------------------"
+                break
+            else
+                echo "$(date +'%F %H:%M:%S') Status: $FIND($STATUS_CHECK)"
+                echo "------------------------------------------------------------------------"
+            fi
+            sleep 3
+        done
+    done 
+}
+
+function installPrometheusOperator () {
+  kubectl create -f $ROOT_FOLDER/prometheus/operator/
+  kubectl get pods -n monitoring | grep 'prom'
+
+  array=("prometheus-operator" )
+  namespace=monitoring
+  export STATUS_SUCCESS="Running"
+  for i in "${array[@]}"
+    do 
+        echo ""
+        echo "------------------------------------------------------------------------"
+        echo "Check $i"
+        while :
+        do
+            FIND=$i
+            STATUS_CHECK=$(kubectl get pods -n $namespace | grep "$FIND" | awk '{print $3;}' | sed 's/"//g' | sed 's/,//g')
+            echo "Status: $STATUS_CHECK"
+            STATUS_VERIFICATION=$(echo "$STATUS_CHECK" | grep $STATUS_SUCCESS)
+            if [ "$STATUS_VERIFICATION" = "$STATUS_SUCCESS" ]; then
+                echo "$(date +'%F %H:%M:%S') Status: $FIND is Ready"
+                echo "------------------------------------------------------------------------"
+                break
+            else
+                echo "$(date +'%F %H:%M:%S') Status: $FIND($STATUS_CHECK)"
+                echo "------------------------------------------------------------------------"
+            fi
+            sleep 3
+        done
+    done 
+}
+
+function createPrometheusInstance () {
+    
+  kubectl create -f $ROOT_FOLDER/prometheus/prometheus/
+
+  kubectl get clusterrolebinding -n monitoring | grep 'prom'
+  kubectl get clusterrole -n monitoring | grep 'prom'
+  kubectl get prometheus -n monitoring 
+  kubectl get pods -n monitoring
+
+  array=("prometheus-prometheus-instance" )
+  namespace=monitoring
+  export STATUS_SUCCESS="Running"
+  for i in "${array[@]}"
+    do 
+        echo ""
+        echo "------------------------------------------------------------------------"
+        echo "Check $i"
+        while :
+        do
+            FIND=$i
+            STATUS_CHECK=$(kubectl get pods -n $namespace | grep "$FIND" | awk '{print $3;}' | sed 's/"//g' | sed 's/,//g')
+            echo "Status: $STATUS_CHECK"
+            STATUS_VERIFICATION=$(echo "$STATUS_CHECK" | grep $STATUS_SUCCESS)
+            if [ "$STATUS_VERIFICATION" = "$STATUS_SUCCESS" ]; then
+                echo "$(date +'%F %H:%M:%S') Status: $FIND is Ready"
+                echo "------------------------------------------------------------------------"
+                break
+            else
+                echo "$(date +'%F %H:%M:%S') Status: $FIND($STATUS_CHECK)"
+                echo "------------------------------------------------------------------------"
+            fi
+            sleep 3
+        done
+    done 
+}
+
+function verifyPrometheusInstance () {
+   kubectl get service -n monitoring
+   kubectl port-forward service/prometheus-instance -n monitoring 9090
+}
+
+# **********************************************************************************
+# Execution
+# **********************************************************************************
+
+echo "************************************"
+echo " Install cert manager"
+echo "************************************"
+installCertManager
+
+echo "************************************"
+echo " Install olm"
+echo "************************************"
+installOLM
+
+echo "************************************"
+echo " Install prometheus operator"
+echo "************************************"
+installPrometheusOperator
+
+echo "************************************"
+echo " Create prometheus instance"
+echo "************************************"
+createPrometheusInstance
+
+echo "************************************"
+echo " Verify prometheus instance"
+echo "************************************"
+verifyPrometheusInstance
