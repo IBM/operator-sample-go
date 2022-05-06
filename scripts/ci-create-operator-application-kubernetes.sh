9e7d0adf57ae4b187448819852cf6472f3fba4c2
thomassuedbroecker <thomas.suedbroecker@de.ibm.com>
2022-05-06 14:07:55 +0200

    add ci for application operator add version_local.env-template add version_local.env to git ignore to avoid override local configurations

---
+++ scripts/ci-create-operator-application-kubernetes.sh
@@ -0,0 +1,344 @@
+#!/bin/bash
+
+# **********************************************************************************
+# Set global variables using parameters
+# **********************************************************************************
+
+echo "************************************"
+echo " Display parameter"
+echo "************************************"
+echo ""
+echo "Parameter count : $@"
+echo "Parameter zero 'name of the script': $0"
+echo "---------------------------------"
+echo "CI Configuration         : $1"
+echo "-----------------------------"
+
+# **************** Global variables
+
+ROOT_FOLDER=$(cd $(dirname $0); cd ..; pwd)
+NAMESPACE=operators
+export CI_CONFIG=$1
+export VERSIONS_FILE=""
+
+# **********************************************************************************
+# Functions
+# **********************************************************************************
+
+function setEnvironmentVariables () {
+
+    if [[ $CI_CONFIG == "local" ]]; then
+        echo "*** Set versions_local.env file a input"
+        source $ROOT_FOLDER/versions_local.env
+        break
+    elif [[ $CI_CONFIG == "ci" ]]; then
+        echo "*** Set versions.env file a input"        
+        source $ROOT_FOLDER/versions.env
+        break
+    else 
+        echo "*** Please select a valid option to run!"
+        echo "*** Use 'local' for your local test."
+        echo "*** Use 'ci' for your the ci test."
+        echo "*** Example:"
+        echo "*** sh ci-operators-kubernetes.sh local"
+        exit 1
+    fi
+}
+
+function verifyPreReqs () {
+  
+  max_retrys=2
+  j=0
+  array=("cert-manager-cainjector" "cert-manager-webhook")
+  namespace=cert-manager
+  export STATUS_SUCCESS="Running"
+  for i in "${array[@]}"
+    do 
+        echo ""
+        echo "------------------------------------------------------------------------"
+        echo "Check $i"
+        while :
+        do
+            FIND=$i
+            ((j++))
+            STATUS_CHECK=$(kubectl get pods -n $namespace | grep "$FIND" | awk '{print $3;}' | sed 's/"//g' | sed 's/,//g')
+            echo "Status: $STATUS_CHECK"
+            STATUS_VERIFICATION=$(echo "$STATUS_CHECK" | grep $STATUS_SUCCESS)
+            if [ "$STATUS_VERIFICATION" = "$STATUS_SUCCESS" ]; then
+                echo "$(date +'%F %H:%M:%S') Status: $FIND is Ready"
+                echo "------------------------------------------------------------------------"
+                break
+            elif [[ $j -eq $max_retrys ]]; then
+                echo "$(date +'%F %H:%M:%S') Please run `install-required-kubernetes-components.sh`first!"
+                echo "$(date +'%F %H:%M:%S') Prereqs aren't ready!"
+                echo "------------------------------------------------------------------------"
+                break               
+            else
+                echo "$(date +'%F %H:%M:%S') Status: $FIND($STATUS_CHECK)"
+                echo "------------------------------------------------------------------------"
+            fi
+            sleep 3
+        done
+    done 
+
+  array=("catalog-operator" "olm-operator" "operatorhubio-catalog" )
+  namespace=olm
+  export STATUS_SUCCESS="Running"
+  for i in "${array[@]}"
+    do 
+        echo ""
+        echo "------------------------------------------------------------------------"
+        echo "Check $i"
+        while :
+        do
+            FIND=$i
+            ((j++))
+            STATUS_CHECK=$(kubectl get pods -n $namespace | grep "$FIND" | awk '{print $3;}' | sed 's/"//g' | sed 's/,//g')
+            echo "Status: $STATUS_CHECK"
+            STATUS_VERIFICATION=$(echo "$STATUS_CHECK" | grep $STATUS_SUCCESS)
+            if [ "$STATUS_VERIFICATION" = "$STATUS_SUCCESS" ]; then
+                echo "$(date +'%F %H:%M:%S') Status: $FIND is Ready"
+                echo "------------------------------------------------------------------------"
+                break
+            elif [[ $j -eq $max_retrys ]]; then
+                echo "$(date +'%F %H:%M:%S') Please run `install-required-kubernetes-components.sh`first!"
+                echo "$(date +'%F %H:%M:%S') Prereqs aren't ready!"
+                echo "------------------------------------------------------------------------"
+                break               
+            else
+                echo "$(date +'%F %H:%M:%S') Status: $FIND($STATUS_CHECK)"
+                echo "------------------------------------------------------------------------"
+            fi
+            sleep 3
+        done
+    done 
+
+  array=("prometheus-operator" )
+  namespace=monitoring
+  export STATUS_SUCCESS="Running"
+   for i in "${array[@]}"
+    do 
+        echo ""
+        echo "------------------------------------------------------------------------"
+        echo "Check $i"
+        while :
+        do
+            FIND=$i
+            ((j++))
+            STATUS_CHECK=$(kubectl get pods -n $namespace | grep "$FIND" | awk '{print $3;}' | sed 's/"//g' | sed 's/,//g')
+            echo "Status: $STATUS_CHECK"
+            STATUS_VERIFICATION=$(echo "$STATUS_CHECK" | grep $STATUS_SUCCESS)
+            if [ "$STATUS_VERIFICATION" = "$STATUS_SUCCESS" ]; then
+                echo "$(date +'%F %H:%M:%S') Status: $FIND is Ready"
+                echo "------------------------------------------------------------------------"
+                break
+            elif [[ $j -eq $max_retrys ]]; then
+                echo "$(date +'%F %H:%M:%S') Please run `install-required-kubernetes-components.sh`first!"
+                echo "$(date +'%F %H:%M:%S') Prereqs aren't ready!"
+                echo "------------------------------------------------------------------------"
+                break               
+            else
+                echo "$(date +'%F %H:%M:%S') Status: $FIND($STATUS_CHECK)"
+                echo "------------------------------------------------------------------------"
+            fi
+            sleep 3
+        done
+    done 
+}
+
+function buildApplicationOperator () {
+    cd $ROOT_FOLDER/operator-application
+    make generate
+    make manifests
+    # Build container
+    # make docker-build IMG="$REGISTRY/$ORG/$IMAGE_APPLICATION_OPERATOR"
+    podman build -t "$REGISTRY/$ORG/$IMAGE_APPLICATION_OPERATOR" .
+    # Push container
+    podman login $REGISTRY
+    podman push "$REGISTRY/$ORG/$IMAGE_APPLICATION_OPERATOR"
+}
+
+function buildDatabaseOperatorBundle () {
+    cd $ROOT_FOLDER/operator-application
+    # Build bundle
+    make bundle IMG="$REGISTRY/$ORG/$IMAGE_APPLICATION_OPERATOR"
+    # Replace CSV and RBAC generate files with customized versions
+    cp -nf $ROOT_FOLDER/scripts/operator-application.clusterserviceversion-TEMPLATE.yaml $ROOT_FOLDER/operator-application/bundle/manifests/operator-application.clusterserviceversion.yaml
+    cp -nf $ROOT_FOLDER/scripts/operator-application-role_binding_patch_TEMPLATE.yaml $ROOT_FOLDER/operator-application/config/rbac/role_binding.yaml
+    cp -nf $ROOT_FOLDER/scripts/operator-application-role_patch_TEMPLATE.yaml $ROOT_FOLDER/operator-application/config/rbac/role.yaml
+    # make bundle-build BUNDLE_IMG="$REGISTRY/$ORG/$IMAGE_APPLICATION_OPERATOR_BUNDLE"
+    podman build -f bundle.Dockerfile -t "$REGISTRY/$ORG/$IMAGE_APPLICATION_OPERATOR_BUNDLE" .
+    
+    # Push container
+    podman login $REGISTRY
+    podman push "$REGISTRY/$ORG/$IMAGE_APPLICATION_OPERATOR_BUNDLE"
+}
+
+function buildDatabaseOperatorCatalog () {
+    cd $ROOT_FOLDER/operator-application
+    # make catalog-build CATALOG_IMG="$REGISTRY/$ORG/$IMAGE_APPLICATION_OPERATOR_CATALOG" BUNDLE_IMGS="$REGISTRY/$ORG/$IMAGE_APPLICATION_OPERATOR_BUNDLE"
+    $ROOT_FOLDER/operator-application/bin/opm index add --build-tool podman --mode semver --tag "$REGISTRY/$ORG/$IMAGE_APPLICATION_OPERATOR_CATALOG" --bundles "$REGISTRY/$ORG/$IMAGE_APPLICATION_OPERATOR_BUNDLE"
+    podman login $REGISTRY
+    podman push "$REGISTRY/$ORG/$IMAGE_APPLICATION_OPERATOR_CATALOG"
+}
+
+function createOLMApplicationOperatorYAMLs () {
+    CATALOG_NAME="$REGISTRY/$ORG/$IMAGE_APPLICATION_OPERATOR_CATALOG"
+    sed "s+APPLICATION_CATALOG_IMAGE+$CATALOG_NAME+g" $ROOT_FOLDER/scripts/kubernetes-application-catalogsource-TEMPLATE.yaml > $ROOT_FOLDER/scripts/kubernetes-database-catalogsource.yaml
+}
+
+function deployDatabaseOperatorOLM () {
+    kubectl create -f $ROOT_FOLDER/scripts/kubernetes-database-catalogsource.yaml
+    kubectl create -f $ROOT_FOLDER/scripts/kubernetes-database-subscription.yaml
+
+    kubectl get catalogsource operator-database-catalog -n $NAMESPACE -oyaml
+    kubectl get subscriptions operator-database-v0-0-1-sub -n $NAMESPACE -oyaml
+    kubectl get installplans -n $NAMESPACE
+    kubectl get pods -n $NAMESPACE
+    kubectl get all -n $NAMESPACE
+
+    array=("operator-database-catalog")
+    namespace=operators
+    export STATUS_SUCCESS="Running"
+    for i in "${array[@]}"
+        do 
+            echo ""
+            echo "------------------------------------------------------------------------"
+            echo "Check $i"
+            while :
+            do
+                FIND=$i
+                STATUS_CHECK=$(kubectl get pods -n $namespace | grep "$FIND" | awk '{print $3;}' | sed 's/"//g' | sed 's/,//g')
+                echo "Status: $STATUS_CHECK"
+                STATUS_VERIFICATION=$(echo "$STATUS_CHECK" | grep $STATUS_SUCCESS)
+                if [ "$STATUS_VERIFICATION" = "$STATUS_SUCCESS" ]; then
+                    echo "$(date +'%F %H:%M:%S') Status: $FIND is Ready"
+                    echo "------------------------------------------------------------------------"
+                    break
+                else
+                    echo "$(date +'%F %H:%M:%S') Status: $FIND($STATUS_CHECK)"
+                    echo "------------------------------------------------------------------------"
+                fi
+                sleep 3
+            done
+        done
+
+    array=("operator-database.v0.0.1")
+    namespace=operators
+    search=installplans
+    export STATUS_SUCCESS="true"
+    for i in "${array[@]}"
+        do 
+            echo ""
+            echo "------------------------------------------------------------------------"
+            echo "Check $i"
+            while :
+            do
+                FIND=$i
+                STATUS_CHECK=$(kubectl get $search -n $namespace | grep "$FIND" | awk '{print $4;}' | sed 's/"//g' | sed 's/,//g')
+                echo "Status: $STATUS_CHECK"
+                STATUS_VERIFICATION=$(echo "$STATUS_CHECK" | grep $STATUS_SUCCESS)
+                if [ "$STATUS_VERIFICATION" = "$STATUS_SUCCESS" ]; then
+                    echo "$(date +'%F %H:%M:%S') Status: $search($STATUS_CHECK)"
+                    echo "------------------------------------------------------------------------"
+                    break
+                else
+                    echo "$(date +'%F %H:%M:%S') Status: $search($STATUS_CHECK)"
+                    echo "------------------------------------------------------------------------"
+                fi
+                sleep 3
+            done
+        done
+
+    array=("operator-database-controller-manager" )
+    namespace=operators
+    export STATUS_SUCCESS="Running"
+    for i in "${array[@]}"
+        do 
+            echo ""
+            echo "------------------------------------------------------------------------"
+            echo "Check $i"
+            while :
+            do
+                FIND=$i
+                STATUS_CHECK=$(kubectl get pods -n $namespace | grep "$FIND" | awk '{print $3;}' | sed 's/"//g' | sed 's/,//g')
+                echo "Status: $STATUS_CHECK"
+                STATUS_VERIFICATION=$(echo "$STATUS_CHECK" | grep $STATUS_SUCCESS)
+                if [ "$STATUS_VERIFICATION" = "$STATUS_SUCCESS" ]; then
+                    echo "$(date +'%F %H:%M:%S') Status: $FIND is Ready"
+                    echo "------------------------------------------------------------------------"
+                    break
+                else
+                    echo "$(date +'%F %H:%M:%S') Status: $FIND($STATUS_CHECK)"
+                    echo "------------------------------------------------------------------------"
+                fi
+                sleep 3
+            done
+        done
+}
+
+function createDatabaseInstance () {
+    kubectl create ns database   
+    kubectl apply -f $ROOT_FOLDER/operator-database/config/samples/database.sample_v1alpha1_database.yaml
+    kubectl apply -f $ROOT_FOLDER/operator-database/config/samples/database.sample_v1alpha1_databasecluster.yaml
+    kubectl get databases.database.sample.third.party/database -n database -oyaml
+    kubectl get databaseclusters.database.sample.third.party/databasecluster-sample -n database -oyaml
+}
+
+function verifyDatabase() {
+     kubectl get databasecluster/databasecluster-sample -oyaml
+     kubectl exec -n database database-cluster-1 -- curl -s http://localhost:8089/persons
+     kubectl exec -n database database-cluster-0 -- curl -s http://localhost:8089/api/leader
+}
+
+# **********************************************************************************
+# Execution
+# **********************************************************************************
+
+echo "************************************"
+echo " Set context"
+echo "************************************"
+setEnvironmentVariables
+
+echo "************************************"
+echo " Verify prerequisites"
+echo "************************************"
+verifyPreReqs
+
+echo "************************************"
+echo " Build 'database operator'"
+echo " Push image to $REGISTRY/$ORG/$IMAGE_DATABASE_OPERATOR"
+echo "************************************"
+buildDatabaseOperator
+
+echo "************************************"
+echo " Build 'database operator bundle'"
+echo " Push image to $REGISTRY/$ORG/$IMAGE_DATABASE_OPERATOR_BUNDLE"
+echo "************************************"
+buildDatabaseOperatorBundle
+
+echo "************************************"
+echo " Build 'application operator catalog'"
+echo " Push image to $REGISTRY/$ORG/$IMAGE_APPLICATION_OPERATOR_CATALOG"
+echo "************************************"
+buildApplicationOperatorCatalog
+
+echo "************************************"
+echo " Create OLM yamls"
+echo "************************************"
+createOLMApplicationOperatorYAMLs
+
+echo "************************************"
+echo " Deploy Application Operator OLM"
+echo "************************************"
+deployApplicationOperatorOLM
+
+echo "************************************"
+echo " Create Application Instance"
+echo "************************************"
+createApplicationInstance
+
+echo "************************************"
+echo " Verify Application Instance"
+echo "************************************"
+verifyApplication
