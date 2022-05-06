55074c76ab7e24edf1c5b837625eeac4b70b85dd
thomassuedbroecker <thomas.suedbroecker@de.ibm.com>
2022-05-06 11:18:15 +0200

    update  ci and demo-setup

--- scripts/demo-setup-kubernetes.sh
+++ scripts/demo-setup-kubernetes.sh
@@ -110,14 +110,84 @@ function verifyPreReqs () {
     done 
 }
 
-function deployDatabaseOperator () {
-    kubectl apply -f $ROOT_FOLDER/operator-database/olm/catalogsource.yaml
-    kubectl get catalogsource -n operators
-    kubectl apply -f $ROOT_FOLDER/operator-database/olm/subscription.yaml
-    kubectl get subscription -n operators
-    kubectl get installplans -n operators
+function createOLMDatabaseOperatorYAMLs () {
+    CATALOG_NAME="$REGISTRY/$ORG/$IMAGE_DATABASE_OPERATOR_CATALOG"
+    sed "s+DATABASE_CATALOG_IMAGE+$CATALOG_NAME+g" $ROOT_FOLDER/scripts/kubernetes-database-catalogsource-TEMPLATE.yaml > $ROOT_FOLDER/scripts/kubernetes-database-catalogsource.yaml
+}
 
-    array=("operator-database-catalog" "operator-database-controller-manager" )
+function deployDatabaseOperator () {
+
+    # kubectl apply -f $ROOT_FOLDER/operator-database/olm/catalogsource.yaml
+    # kubectl get catalogsource -n operators
+    # kubectl apply -f $ROOT_FOLDER/operator-database/olm/subscription.yaml
+    # kubectl get subscription -n operators
+    # kubectl get installplans -n operators
+
+    kubectl create -f $ROOT_FOLDER/scripts/kubernetes-database-catalogsource.yaml
+    kubectl create -f $ROOT_FOLDER/scripts/kubernetes-database-subscription.yaml
+    
+    NAMESPACE=operators
+
+    kubectl get catalogsource operator-database-catalog -n $NAMESPACE -oyaml
+    kubectl get subscriptions operator-database-v0-0-1-sub -n $NAMESPACE -oyaml
+    kubectl get installplans -n $NAMESPACE
+    kubectl get pods -n $NAMESPACE
+    kubectl get all -n $NAMESPACE
+
+    array=("operator-database-catalog")
+    namespace=operators
+    export STATUS_SUCCESS="Running"
+    for i in "${array[@]}"
+        do 
+            echo ""
+            echo "------------------------------------------------------------------------"
+            echo "Check $i"
+            while :
+            do
+                FIND=$i
+                STATUS_CHECK=$(kubectl get pods -n $namespace | grep "$FIND" | awk '{print $3;}' | sed 's/"//g' | sed 's/,//g')
+                echo "Status: $STATUS_CHECK"
+                STATUS_VERIFICATION=$(echo "$STATUS_CHECK" | grep $STATUS_SUCCESS)
+                if [ "$STATUS_VERIFICATION" = "$STATUS_SUCCESS" ]; then
+                    echo "$(date +'%F %H:%M:%S') Status: $FIND is Ready"
+                    echo "------------------------------------------------------------------------"
+                    break
+                else
+                    echo "$(date +'%F %H:%M:%S') Status: $FIND($STATUS_CHECK)"
+                    echo "------------------------------------------------------------------------"
+                fi
+                sleep 3
+            done
+        done
+
+    array=("operator-database.v0.0.1")
+    namespace=operators
+    search=installplans
+    export STATUS_SUCCESS="true"
+    for i in "${array[@]}"
+        do 
+            echo ""
+            echo "------------------------------------------------------------------------"
+            echo "Check $i"
+            while :
+            do
+                FIND=$i
+                STATUS_CHECK=$(kubectl get $search -n $namespace | grep "$FIND" | awk '{print $4;}' | sed 's/"//g' | sed 's/,//g')
+                echo "Status: $STATUS_CHECK"
+                STATUS_VERIFICATION=$(echo "$STATUS_CHECK" | grep $STATUS_SUCCESS)
+                if [ "$STATUS_VERIFICATION" = "$STATUS_SUCCESS" ]; then
+                    echo "$(date +'%F %H:%M:%S') Status: $search($STATUS_CHECK)"
+                    echo "------------------------------------------------------------------------"
+                    break
+                else
+                    echo "$(date +'%F %H:%M:%S') Status: $search($STATUS_CHECK)"
+                    echo "------------------------------------------------------------------------"
+                fi
+                sleep 3
+            done
+        done
+
+    array=("operator-database-controller-manager" )
     namespace=operators
     export STATUS_SUCCESS="Running"
     for i in "${array[@]}"
@@ -200,6 +270,11 @@ echo "************************************"
 verifyPreReqs
 
 echo "************************************"
+echo " Create OLM database operator YAMLs"
+echo "************************************"
+createOLMDatabaseOperatorYAMLs
+
+echo "************************************"
 echo " Deploy Database Operator"
 echo "************************************"
 deployDatabaseOperator
@@ -207,14 +282,14 @@ deployDatabaseOperator
 echo "************************************"
 echo " Deploy Application Operator"
 echo "************************************"
-deployApplicationOperator
+# deployApplicationOperator
 
 echo "************************************"
 echo " Create Application and Database instance"
 echo "************************************"
-createApplicationAndDatabaseInstance
+# createApplicationAndDatabaseInstance
 
 echo "************************************"
 echo " Verify Application Beta"
 echo "************************************"
-verifyApplicationBeta
+# verifyApplicationBeta
