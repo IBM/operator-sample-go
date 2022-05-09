a87c97690062b4f7b3e4f5b7f367376b075d5f11
thomassuedbroecker <thomas.suedbroecker@de.ibm.com>
2022-05-09 22:23:41 +0200

    small change automation

--- scripts/ci-create-operators-kubernetes.sh
+++ scripts/ci-create-operators-kubernetes.sh
@@ -76,7 +76,7 @@ function duration() {
     echo "*** End: $end"
     seconds=$(echo "$end - $start" | bc)
     echo $seconds' sec'
-    SCRIPT_DURATION=$(awk -v t=$seconds 'BEGIN{t=int(t*1000); printf "%d:%02d:%02d\n", t/3600000, t/60000%60, t/1000%60}')
+    export SCRIPT_DURATION=$(awk -v t=$seconds 'BEGIN{t=int(t*1000); printf "%d:%02d:%02d\n", t/3600000, t/60000%60, t/1000%60}')
     echo "Formatted: $SCRIPT_DURATION"
     
 }
