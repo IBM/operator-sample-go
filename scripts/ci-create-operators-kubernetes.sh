5d9019eb3527c5691c2ba11bf3d83129f55e7e74
thomassuedbroecker <thomas.suedbroecker@de.ibm.com>
2022-05-10 08:32:34 +0200

    update ci create script

--- scripts/ci-create-operators-kubernetes.sh
+++ scripts/ci-create-operators-kubernetes.sh
@@ -43,6 +43,11 @@ function setupDatabase () {
 
 function setupApplication () {
     bash "$ROOT_FOLDER/scripts/ci-create-operator-application-kubernetes.sh" $CI_CONFIG $RESET $RESET_PODMAN
+        if [ $? == "1" ]; then
+        echo "*** The setup of the applicatiob-operator failed !"
+        echo "*** The script 'ce-create-operators-kubernetes.sh' ends here!"
+        exit 1
+    fi
 }
 
 function run () {
